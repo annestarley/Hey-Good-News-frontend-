822d6b85e728ad5188c56cc02de23cf92fcd085e
Anne Starley <annestarley@gmail.com>
2018-04-04 14:33:26 -0700

    basic outline for tones and article

--- src/components/news/Article.js
+++ src/components/news/Article.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import MoreTonesToggle from './MoreTonesToggle'
 
 const Article = () => {
   return (
@@ -7,10 +8,10 @@ const Article = () => {
       <p className="source-time">The New York Times - Published 2018-04-04T16:36:53Z</p>
       <p className="description">The ratings for the show’s second night are down from its surprising premiere, but still strong enough for ABC to call it a hit.</p>
       <div className="tones">
-        <p><span className="top-tones">joy 53.82% </span><span className="top-tones">tentative 65.25%</span> <span className="top-tones">openness 68.24%</span></p>
-        <p><strong>Emotional:</strong> anger 10.01%, disgust 8.99%, fear 10.94%, joy 53.82%</p>
-        <p><strong>Language:</strong> analytical 53.81%, confident 0.00%, tentative 65.25%</p>
-        <p><strong>Social:</strong> agreeableness 26.81%, conscientiousness 57.00%, emotionalRange 66.72%, extraversion 52.40%, openness 68.24%</p>
+        <p><span className="top-tones">Top tones:</span> <span className="top-tones">joy 53.82% </span><span className="top-tones">tentative 65.25%</span> <span className="top-tones">openness 68.24%</span></p>
+        <div id="read-more-toggle">
+          <MoreTonesToggle />
+        </div>
       </div>
 
       <br></br>
---
+++ src/components/news/MoreTonesToggle.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Collapse } from 'mdbreact';
+
+class MoreTonesToggle extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      collapse: false,
+    };
+  }
+
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <a href="#" onClick={this.toggle}>Read more...</a>
+          <Collapse isOpen={this.state.collapse}>
+            <div>
+              <p id="other-tones"><strong>Emotional:</strong> anger 10.01%, disgust 8.99%, fear 10.94%, joy 53.82%</p>
+              <p><strong>Language:</strong> analytical 53.81%, confident 0.00%, tentative 65.25%</p>
+              <p><strong>Social:</strong> agreeableness 26.81%, conscientiousness 57.00%, emotionalRange 66.72%, extraversion 52.40%, openness 68.24%</p>
+            </div>
+          </Collapse>
+        </div>
+      </div>
+    );
+  }
+}
+export default MoreTonesToggle;
