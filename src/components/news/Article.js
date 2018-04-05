b9d59191a30d32ec967373b5624f2070e0b06195
Anne Starley <annestarley@gmail.com>
2018-04-05 11:48:26 -0700

    added messy published ... ago code

--- src/components/news/Article.js
+++ src/components/news/Article.js
@@ -11,7 +11,7 @@ const Article = (props) => {
   }
   if (parseFloat(props.tone.emotion.fear) > parseFloat(topEmotionalTone)) {
     topEmotionalTone = props.tone.emotion.fear
-    topEmotionalToneName = 'disgust'
+    topEmotionalToneName = 'fear'
   }
   if (parseFloat(props.tone.emotion.joy) > parseFloat(topEmotionalTone)) {
     topEmotionalTone = props.tone.emotion.joy
@@ -47,16 +47,41 @@ const Article = (props) => {
     topSocialTone = props.tone.social.openness
     topSocialToneName = 'openness'
   }
-  //
-  // function openInNewTab(url) {
-  //   var win = window.open(url, '_blank');
-  //   win.focus();
-  // }
+
+
+  var date = new Date().toUTCString().split(' ')
+  let currentTime = date[4].split(':')
+
+  let propsTime = props.publishedAt.toString().split('T')
+  let publishedTime = propsTime[1].split('Z')
+  publishedTime = publishedTime[0].split(':')
+
+  let hours = currentTime[0] - publishedTime[0]
+  let mins = currentTime[1] - publishedTime[1]
+  let secs = currentTime[2] - publishedTime[2]
+
+  let secondsTotal = hours*3600 + mins*60 + secs
+  let hoursFinal = Math.floor(secondsTotal/3600) - 1
+  let minutes = secondsTotal%3600
+  minutes = Math.floor(minutes/60)
+  let seconds = minutes%60
+
+  if (hoursFinal == 0) hoursFinal = ''
+  else if (hoursFinal == 1) hoursFinal = '1 hour'
+  else hoursFinal = `${hoursFinal} hours`
+  if (minutes == 0) minutes = ''
+  else if (minutes == 1) minutes = '1 minute'
+  else minutes = `${minutes} minutes`
+  if (seconds == 0) seconds = ''
+  else if (seconds == 1) seconds = '1 secs'
+  else seconds = `${seconds} secs`
+
+  let publishedTimeDifference = `${hoursFinal} ${minutes} ${seconds} ago`
 
   return (
     <div>
-      <a style={{display: "table-cell"}} href="https://www.nytimes.com/2018/04/04/business/media/roseanne-ratings-abc.html" target="_blank"><h4><strong>{props.title}</strong></h4></a>
-      <p className="source-time">{props.source.name} - Published {props.publishedAt}</p>
+      <a style={{display: "table-cell"}} href={props.url} target="_blank"><h4><strong>{props.title}</strong></h4></a>
+      <p className="source-time">{props.source.name} - Published {publishedTimeDifference}</p>
       <p className="description">{props.description}</p>
       <div className="tones">
         <p>
