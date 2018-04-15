dff4d3f73884499693417de6ecf0912fc5b1a08f
Anne Starley <annestarley@gmail.com>
2018-04-15 16:53:06 -0700

    get username and password route works

--- src/components/signup/SignupModal.js
+++ src/components/signup/SignupModal.js
@@ -1,22 +1,53 @@
 
 import React from 'react';
 import { Button, Modal, ModalBody, ModalHeader, ModalFooter, Input } from 'mdbreact';
+import axios from 'axios'
 
+const backendURL = 'http://localhost:8000'
 
 class SignupModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
+      username: '',
+      email: ''
     };
 
     this.toggle = this.toggle.bind(this);
   }
 
   toggle() {
+    console.log('togge ran')
     this.setState({
       modal: !this.state.modal
     });
+  }
+
+  getUsername = (e) => {
+    console.log('username', e.target.value)
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  getEmail = (e) => {
+    console.log('email', e.target.value)
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  postUser = () => {
+    console.log('post user ran!')
+    axios.post(`${backendURL}/userInfo/`, {username: this.state.username, email: this.state.email})
+      .then(res => {
+        console.log('response from backend', res)
+        this.toggle()
+      })
+      .catch(err => {
+        console.log('err from server', err)
+      })
   }
 
   render() {
@@ -27,12 +58,14 @@ class SignupModal extends React.Component {
           <ModalHeader toggle={this.toggle}>Daily Good News Sign Up</ModalHeader>
           <ModalBody>
             <p>Enter your first name and email address and we will send you a daily article where joy is the top tone.</p>
-            <Input label="Name" icon="user" className="signup-icon"/>
-            <Input label="Email address" icon="envelope" className="signup-icon"/>
+            <Input label="Name" icon="user" className="signup-icon" onChange={this.getUsername}/>
+            <Input label="Email address" icon="envelope" className="signup-icon" onChange={this.getEmail}/>
           </ModalBody>
           <ModalFooter>
             <Button color="deep-orange lighten-1" onClick={this.toggle}>Close</Button>{' '}
-            <Button color="mdb-color darken-2" onClick={this.toggle}>Save changes</Button>
+            {/*<Button color="mdb-color darken-2" onClick={this.toggle} onSubmit={this.postUser}>Save changes</Button>*/}
+            <Button color="mdb-color darken-2" onClick={this.postUser}>Save changes</Button>
+
           </ModalFooter>
         </Modal>
       </div>
---
+++ src/components/signup/SuccessMessage.js
@@ -0,0 +1,57 @@
+
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import './alerts.css';
+import { Button } from 'mdbreact';
+
+class AlertPage extends Component {
+  notify(type){
+  return () => {
+    switch (type) {
+      case 'info':
+        toast.info('Info message', {
+          autoClose: 3000
+        });
+        break;
+      case 'success':
+        toast.success('Success message', {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+        break;
+      case 'warning':
+        toast.warn('Warning message');
+        break;
+      case 'error':
+        toast.error('Error message');
+        break;
+    }
+  };
+};
+  render(){
+    return (
+      <div>
+      <button className='btn btn-info'
+        onClick={this.notify('info')}>Info
+      </button>
+      <hr/>
+      <button className='btn btn-success'
+        onClick={this.notify('success')}>Success
+      </button>
+      <hr/>
+      <button className='btn btn-warning'
+        onClick={this.notify('warning')}>Warning
+      </button>
+      <hr/>
+      <button className='btn btn-danger'
+        onClick={this.notify('error')}>Error
+      </button>
+      <ToastContainer
+        hideProgressBar={true}
+        newestOnTop={true}
+        autoClose={5000}
+      />
+      </div>
+    );
+  }
+}
+export default AlertPage;
